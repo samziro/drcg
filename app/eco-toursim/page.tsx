e73ac4889fe53c11e48777b2113bf272c1370a14
wanje ziro <samuelziro76@gmail.com>
2026-05-09 05:15:35 -0700

    first commit

--- app/eco-toursim/page.tsx
+++ app/eco-toursim/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <h1 className='text-3xl font-bold text-gray-900'>Coming soon</h1>
+        <h1 className='text-3xl font-bold text-gray-900 flex items-center'>Coming soon</h1>
     </div>
   )
 }
